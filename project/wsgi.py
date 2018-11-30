9b0f7e136ef5e130f3edd66387d627d1552b725f
kalumot <you@example.com>
2018-11-30 19:00:32 +0000

    testing

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,8 +8,23 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import time
+import traceback
+import signal
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+try:
+    application = get_wsgi_application()
+    print 'WSGI without exception'
+except Exception:
+    print 'handling WSGI exception'
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
