850004bddb733412c0e1c98d48ea10ae9485fad2
kalumot <you@example.com>
2018-12-02 16:50:03 +0000

    Testing

--- ammamanager/urls.py
+++ ammamanager/urls.py
@@ -5,17 +5,17 @@ from .views import ammamanager, gyms, promotions
 urlpatterns = [
     path('', ammamanager.home, name='home'),
 
-    path('gyms/', include(([
-        path('', gyms.QuizListView.as_view(), name='quiz_list'),
-        path('fighters/', gyms.ListFightersView.as_view(), name='fighter_list'),
-        path('fighters/add/', gyms.FighterCreateView.as_view(), name='fighter_add'),
-    ], 'ammamanager'), namespace='gyms')),
+    #path('gyms/', include(([
+     #   path('', gyms.QuizListView.as_view(), name='quiz_list'),
+      #  path('fighters/', gyms.ListFightersView.as_view(), name='fighter_list'),
+       # path('fighters/add/', gyms.FighterCreateView.as_view(), name='fighter_add'),
+    #], 'ammamanager'), namespace='gyms')),
 
 
 
 
 
-    path('promotions/', include(([
-        path('', promotions.QuizListView.as_view(), name='quiz_change_list'),
-    ], 'ammamanager'), namespace='promotions')),
+    #path('promotions/', include(([
+     #   path('', promotions.QuizListView.as_view(), name='quiz_change_list'),
+    #], 'ammamanager'), namespace='promotions')),
 ]
--- project/urls.py
+++ project/urls.py
@@ -4,8 +4,8 @@ from ammamanager.views import ammamanager, gyms, promotions
 
 urlpatterns = [
     path('', include('ammamanager.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup/', ammamanager.SignUpView.as_view(), name='signup'),
-    path('accounts/signup/gym/', gyms.GymSignUpView.as_view(), name='gym_signup'),
-    path('accounts/signup/promotion/', promotions.PromotionSignUpView.as_view(), name='promotion_signup'),
+    #path('accounts/', include('django.contrib.auth.urls')),
+    #path('accounts/signup/', ammamanager.SignUpView.as_view(), name='signup'),
+    #path('accounts/signup/gym/', gyms.GymSignUpView.as_view(), name='gym_signup'),
+    #path('accounts/signup/promotion/', promotions.PromotionSignUpView.as_view(), name='promotion_signup'),
 ]
