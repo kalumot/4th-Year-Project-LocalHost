256d2d4a9b57e6dbb400242f4d1f2054926b20cd
kalumot <you@example.com>
2018-12-02 01:32:13 +0000

    Testing

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'ammamanager'
+
+    'crispy_forms',
+
+    'ammamanager',
 ]
 
 MIDDLEWARE = [
